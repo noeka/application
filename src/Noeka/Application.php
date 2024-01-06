b19ed56c9a05d359df812a1212c62c03a503b0c5
Mathias N. Kastrup <mathias@noeka.dk>
2024-01-07 00:35:44 +0100

    pluggable container

--- src/Noeka/Application.php
+++ src/Noeka/Application.php
@@ -8,8 +8,18 @@ use Noeka\DI\Container;
 class Application {
     protected ContainerInterface $container;
 
-    public function __construct()
+    public function __construct(?ContainerInterface $container) {
+        $this->resolveContainer($container);
+    }
+
+    protected function resolveContainer(?ContainerInterface $container): void
     {
+        if ($container instanceof ContainerInterface) {
+            $this->container = $container;
+
+            return;
+        }
+
         $this->container = new Container;
     }
 }
